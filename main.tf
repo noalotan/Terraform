39bc0071ec276c0f4f87303ffdb1b905343f729a
noalotan <noalotan1@gmail.com>
2024-09-18 13:42:08 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,10 +14,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_id
+  ami             = var.ami
+  instance_type   = var.instance_type
+  subnet_id       = var.subnet_id
   security_groups = var.security_groups
+  key_name        = var.key_name
 
   tags = {
     Name = var.instance_name
