00ee552ccee2f89e95a259392fcc7148a196eb7f
noalotan <noalotan1@gmail.com>
2024-09-18 13:39:02 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,37 @@
+variable "region" {
+  description = "Value of the region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ami" {
+  description = "Value of the ami for the EC2 instance"
+  type        = string
+  default     = "ami-0a0e5d9c7acc336f1"
+}
+
+variable "instance_type" {
+  description = "Value of the instance type for the EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "subnet_id" {
+  description = "Value of the subnet id for the EC2 instance"
+  type        = string
+  default     = "subnet_id"
+}
+
+variable "security_groups" {
+  description = "Value of the security groups for the EC2 instance"
+  type        = string
+  default     = "security_groups"
+}
+
 variable "instance_name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
   default     = "ExampleAppServerInstance"
 }
+
 
